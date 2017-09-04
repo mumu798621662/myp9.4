048dec38dd24965948dea061605c5fe03c241815
mumu <798621662@qq.com>
2017-09-04 19:07:18 +0800

    'da

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: 木木
+* @Date:   2017-09-04 11:08:29
+* @Last Modified by:   木木
+* @Last Modified time: 2017-09-04 18:14:27
+*/
+var num1=43;
+var num2=40;
+var info="我们班有两个班组成，第一个班505班有"+num1+"人，"+"第二个班507班有"+num2+"人。总共有"+num1-num2+"人。"
+document.write(info);
